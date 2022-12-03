db95c209f887833e4965f8e4e29020207c6cbfac
Adrine UWERA <>
2022-12-03 11:43:22 +0200

    configured tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js, jsx}',
+    './src/Components/**/*.{js, jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
